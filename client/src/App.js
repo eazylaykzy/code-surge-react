370873e024a831a7792553dc81d640c6f8811490
Eazylaykzy <eazylaykzy@gmail.com>
2019-12-10 14:17:17 -0800

    Axios fix

--- client/src/App.js
+++ client/src/App.js
@@ -138,7 +138,7 @@ const App = ({location: {pathname}}) => {
 		e.preventDefault();
 		axios({
 			method: "POST",
-			url: `https://codesurge.herokuapp.com:3002/contact`,
+			url: `http://localhost:3002/contact`,
 			data: {
 				name,
 				email,
