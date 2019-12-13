93d9e2496e7c6deb9b602bbda5a13a3a95385a40
Eazylaykzy <eazylaykzy@gmail.com>
2019-12-12 17:53:41 -0800

    Resolves issues with local Git History.

--- client/src/components/button/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './button.scss';
-
-export const Button = props => (
-	<div>
-		<a target={props.target} className={props.classProps} href={props.url}>{props.children}</a>
-	</div>
-);
